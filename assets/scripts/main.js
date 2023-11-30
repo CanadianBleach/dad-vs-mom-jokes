c6a0a7553993719fbe770d2dfe853d120f017966
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-29 21:28:39 -0500

    added first api

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,6 @@ import { fetchData } from "../utils/utils.js";
 
 let momJoke = document.querySelector("#mom-joke");
 let dadJoke = document.querySelector("#dad-joke");
+
+let data = await fetchData("https://api.yomomma.info/");
+console.log(data);

52a72e458456a60d26866ea175bd6a191d7438d9
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2023-11-30 19:28:19 -0500

    updated main.js, htlml, utils.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,19 @@
-import { fetchData } from "../utils/utils.js";
+//import { fetchData } from "../utils/utils.js";
 
 let momJoke = document.querySelector("#mom-joke");
 let dadJoke = document.querySelector("#dad-joke");
 
-let data = await fetchData("https://api.yomomma.info/");
-console.log(data);
+/*let data = await fetchData("https://icanhazdadjoke.com/");
+console.log(data);*/
+
+fetch("https://icanhazdadjoke.com/",{
+    headers:{
+        "Accept":"application/json"
+    }
+})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    })

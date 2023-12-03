963e3b6abd89e0b3418b4dae750689e363cb3fbc
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2023-12-02 20:59:41 -0500

    Slight cleanup
    Also added new joke api

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,37 +1,52 @@
 import { fetchData } from "../utils/utils.js";
 
-let momJokes = [];
+let jokes = [];
 let dadJokes = [];
 
-let momJoke = document.querySelector("#mom-joke");
-let dadJoke = document.querySelector("#dad-joke");
+let jokeElement = document.querySelector("#joke");
+let dadJokeElement = document.querySelector("#dad-joke");
 
-momJoke.addEventListener("click", buttonPressed);
-dadJoke.addEventListener("click", buttonPressed);
+// Get and set jokes
+let jokeResp = await fetchData("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single");
+let dadJokeResp = await fetchData("https://icanhazdadjoke.com/");
+// Set joke text, and get new jokes ready
+// This prevents a delay in the jokes changing
+refreshJokeText();
+refreshJokes();
 
-refreshButtons();
-
-function buttonPressed() {
-  updateJokes(document.activeElement.textContent, document.activeElement.id);
-  refreshButtons();
-  console.log(momJokes, dadJokes);
+function refreshJokeText() {
+  jokeElement.textContent = jokeResp.joke;
+  dadJokeElement.textContent = dadJokeResp.joke;
 }
+
+// Get new jokes to add
+async function refreshJokes() {
+  jokeResp = await fetchData("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single");
+  dadJokeResp = await fetchData("https://icanhazdadjoke.com/");
+}
+
+// Log joke selected
+function buttonPressed() {
+  // Add winning jokes to array
+  // Adds via textContent so we must do it before we change the text
+  addJoke(document.activeElement.textContent, document.activeElement.id);
+
+  refreshJokeText();
+  refreshJokes();
+}
+
 // Update the count
-function updateJokes(joke, id) {
-  if (id = "mom-joke") {
-    momJokes.unshift(joke);
+function addJoke(joke, id) {
+  if (id = "joke") {
+    jokes.unshift(joke);
   } else {
     dadJokes.unshift(joke);
   }
 }
-// Refresh the buttons
-async function refreshButtons() {
-  let dadRes = await fetchData("https://icanhazdadjoke.com/");
 
-dadJoke.textContent = dadRes.joke;
-
-let momRes = await fetchData("https://icanhazdadjoke.com/");
-
-momJoke.textContent = momRes.joke;
-  
+function init() {
+  jokeElement.addEventListener("click", buttonPressed);
+  dadJokeElement.addEventListener("click", buttonPressed);
 }
+
+init();

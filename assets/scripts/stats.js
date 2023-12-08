edb7553a97059d7dbd57379465a539c7411c2afa
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2023-12-07 19:57:04 -0500

    Speed Up Fade

--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -38,7 +38,7 @@ function clearSaved() {
         element.classList.add("is-size-3", "visible");
         element.textContent = "No questions to display. Go laugh at our jokes now..."
         noneParent.append(element);
-    }, 1000);
+    }, 750);
 }
 
 function init() {

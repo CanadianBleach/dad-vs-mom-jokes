cecfaebc84325ae400228716422752019b81a6aa
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2023-12-07 20:00:29 -0500

    Update stats.js

--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -38,7 +38,7 @@ function clearSaved() {
         element.classList.add("is-size-3", "visible");
         element.textContent = "No questions to display. Go laugh at our jokes now..."
         noneParent.append(element);
-    }, 750);
+    }, 350);
 }
 
 function init() {
